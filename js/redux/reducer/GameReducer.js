bf47e5c8e13a590c07df203e1ecfba2c29e354ca
Kevin Hubert <>
2019-01-22 00:51:04 +0100

    Add 'GameReducer.js' which will be used to store the choice made by the human-player and get a random choice for the bot in HumanVsBot-matches. In BotVsBot-machtes both choices will be randomly set. Choices are a numeric-value 0-2 which indicated the 'rock'-, 'paper'- or 'scissor'-selection
    The imported "BotAPI.js" is currently a set of functions which return hardcoded values for testing-purpose, will be commited with correct functionality later.

---
+++ js/redux/reducer/GameReducer.js
@@ -0,0 +1,68 @@
+// TODO: Add real BotAPI-logic currently only returns hardcoded values
+import * as BotAPI from '../../bot.js'
+
+/* Dispatchable actions */
+export const HUMAN_CHOICE_CHANGED = 'HUMAN_CHOICE_CHANGED'
+export const BVB_RANDOM_CHOICES = 'BVB_RANDOM_CHOICES'
+
+/* Init object to use when reducer get called for the first time */
+const defaultGameState = {
+  humanVsBot: {
+    humanChoice: undefined,
+    botChoice: undefined,
+    result: undefined
+  },
+  botVsBot: {
+    bot1Choice: undefined,
+    bot2Choice: undefined,
+    result: undefined
+  }
+}
+
+/**
+ * GameReducer. Reducer-function to call when a action get dispatched
+ * @param {*} state Current state of the store.
+ * @param {*} action Action which was dispatched
+ */
+export const reducer = (state = defaultGameState, action) => {
+  switch (action.type) {
+    /* Set choice of player and randomly generate a bot-choice. Human vs bot functionality. */
+    case HUMAN_CHOICE_CHANGED: {
+      const botChoice = BotAPI.getRandomChoice();
+      const result = BotAPI.getWinner(action.choice, botChoice)
+
+      return {
+        ...state,
+        humanVsBot: {
+          humanChoice: action.choice,
+          botChoice,
+        },
+        lastResult: {
+          ...result,
+          type: BotAPI.gameModus.HUMAN_VS_BOT
+        }
+      }
+    }
+    /* Randomly get a choice for both bots. => Bot vs bot functionality. */
+    case BVB_RANDOM_CHOICES: {
+      const bot1Choice = BotAPI.getRandomChoice()
+      const bot2Choice = BotAPI.getRandomChoice()
+      const result = BotAPI.getWinner(bot1Choice, bot2Choice)
+
+      return {
+        ...state,
+        botVsBot: {
+          bot1Choice,
+          bot2Choice,
+        },
+        lastResult: {
+          ...result,
+          type: BotAPI.gameModus.BOT_VS_BOT
+        }
+      }
+    }
+    default: {
+      return state;
+    }
+  }
+}
